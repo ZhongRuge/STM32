8e444222d63fbb4a0a782a72d79d7292184255fd
Zhongruge <635246203@qq.com>
2025-10-26 21:19:07 +0800

    完成光敏电阻更改蜂鸣器模块

--- 3-5 Light control Buzz/User/main.c
+++ 3-5 Light control Buzz/User/main.c
@@ -1,25 +1,30 @@
 #include <stm32f10x.h>
 #include <stm32f10x_conf.h>
-#include "LED.h"
-#include "Button.h"
-
-uint8_t keyNum;
 
 
 int main(void)
 {
-	LED_Init();
-	Button_Init();
-	LED_SWITCH(GPIOA, GPIO_Pin_1 | GPIO_Pin_11, Bit_RESET);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
 	while (1){
-		keyNum = Button_GetNum();
-		if (keyNum == 1){
-			LED_SWITCH(GPIOA, GPIO_Pin_1, Bit_SET);
-			LED_SWITCH(GPIOA, GPIO_Pin_2, Bit_RESET);
+
+		if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_13) == 0){
+			GPIO_ResetBits(GPIOB, GPIO_Pin_12);
 		}
-		else if (keyNum == 2){
-			LED_SWITCH(GPIOA, GPIO_Pin_2, Bit_SET);
-			LED_SWITCH(GPIOA, GPIO_Pin_1, Bit_RESET);
+		else{
+			GPIO_SetBits(GPIOB, GPIO_Pin_12);
 		}
+
 	}
 }
